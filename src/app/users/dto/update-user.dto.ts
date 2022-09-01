bb12ea17360eb8cb596d630ec013d515fb0e03f5
anevaraz <danielgutierrez.info@gmail.com>
2022-09-01 09:37:31 -0300

    refactor update user dto

--- src/app/users/dto/update-user.dto.ts
+++ src/app/users/dto/update-user.dto.ts
@@ -1,4 +1,33 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateUserDto } from './create-user.dto';
+import {
+  IsEnum,
+  IsOptional,
+  IsPhoneNumber,
+  Length,
+  Matches,
+} from 'class-validator';
+import { MessagesHelper } from '../../../helpers/messages.helper';
+import { RegexHelper } from '../../../helpers/regex.helper';
+import { UserGenderEnum } from '../enum/user-gender.enum';
+export class UpdateUserDto {
+  @IsOptional()
+  @Length(3, 20)
+  firstName?: string;
 
-export class UpdateUserDto extends PartialType(CreateUserDto) {}
+  @IsOptional()
+  @Length(3, 100)
+  lastName?: string;
+
+  @IsOptional()
+  @Matches(new RegExp(RegexHelper.DATE_OF_BIRTH), {
+    message: MessagesHelper.DATE_OF_BIRTH_VALID,
+  })
+  dateOfBirth?: string;
+
+  @IsOptional()
+  @IsEnum([UserGenderEnum.FEMALE, UserGenderEnum.MALE])
+  gender?: string;
+
+  @IsOptional()
+  @IsPhoneNumber('BR')
+  phoneNumber?: string;
+}
