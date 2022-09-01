71d14ab1acbd8f852cb7275ccabd4a17fec3bfc9
anevaraz <danielgutierrez.info@gmail.com>
2022-08-31 21:20:33 -0300

    added typeorm options type

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,22 +1,22 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { TypeOrmModule } from '@nestjs/typeorm';
+import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
 import { CustomerModule } from './modules/customer/customer.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
     TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: process.env.DB_HOST,
-      port: Number(process.env.DB_PORT),
-      database: process.env.DB_DATABASE,
-      username: process.env.DB_USERNAME,
-      password: process.env.DB_PASSWORD,
+      type: process.env.TYPEORM_CONNECTION,
+      host: process.env.TYPEORM_HOST,
+      port: Number(process.env.TYPEORM_PORT),
+      database: process.env.TYPEORM_DATABASE,
+      username: process.env.TYPEORM_USERNAME,
+      password: process.env.TYPEORM_PASSWORD,
       synchronize: process.env.NODE_ENV === 'development' ? true : false,
       logging: process.env.NODE_ENV === 'development' ? true : false,
       entities: [`${__dirname}/**/*.entity{.js,.ts}`],
-    }),
+    } as TypeOrmModuleOptions),
     CustomerModule,
   ],
   controllers: [],
