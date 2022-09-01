a2f87e584e0a8eaf67d7d07bec026e546b9a5f66
anevaraz <danielgutierrez.info@gmail.com>
2022-09-01 11:43:57 -0300

    remove unused message and fix message

--- src/helpers/messages.helper.ts
+++ src/helpers/messages.helper.ts
@@ -1,7 +1,6 @@
 export const MessagesHelper = {
   PASSWORD_VALID:
-    'password must contain lowercase letters, numbers and special characters',
+    'password must contain uppercase and lowercase letters, numbers and special characters',
   PASSWORD_OR_EMAIL_INVALID: 'email and/or password are invalid',
   DATE_OF_BIRTH_VALID: 'date must be standard format DD/MM/YYYY',
-  DATE_OF_BIRTH_INVALID: 'date is not in DD/MM/YYYY format',
 };
