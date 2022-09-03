b7d3cc89dfb1db67ad35865e620220749cdd820e
anevaraz <danielgutierrez.info@gmail.com>
2022-09-03 14:47:11 -0300

    added not found message

--- src/helpers/messages.helper.ts
+++ src/helpers/messages.helper.ts
@@ -3,4 +3,5 @@ export const MessagesHelper = {
     'password must contain uppercase and lowercase letters, numbers and special characters',
   PASSWORD_OR_EMAIL_INVALID: 'email and/or password are invalid',
   DATE_OF_BIRTH_VALID: 'date must be standard format DD/MM/YYYY',
+  NOT_FOUND: 'user not found',
 };
