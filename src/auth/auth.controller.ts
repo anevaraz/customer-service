15848d69f98c9bc71d0bddd12e8c8f1fe5b75dd4
anevaraz <danielgutierrez.info@gmail.com>
2022-09-03 18:24:33 -0300

    set type response swagger

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -4,6 +4,7 @@ import { ApiBody, ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { UnauthorizedUserSwagger } from './swagger/unauthorized.auth.swagger';
 import { AuthService } from './auth.service';
 import { LoginUserDto } from '../app/users/dto/login-user.dto';
+import { TokenAuhtSwagger } from './swagger/token.auth.swagger';
 
 @Controller('v1/auth')
 @ApiTags('Auth')
@@ -13,7 +14,7 @@ export class AuthController {
   @Post('login')
   @ApiBody({ type: LoginUserDto })
   @ApiOperation({ summary: 'users login to generate token' })
-  @ApiResponse({ status: 201, description: 'token generated' })
+  @ApiResponse({ status: 201, type: TokenAuhtSwagger })
   @ApiResponse({ status: 401, type: UnauthorizedUserSwagger })
   async login(@Req() req: any) {
     return await this.authService.login(req.user);
