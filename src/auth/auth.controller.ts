77e3dede982631329c824395571df6a730140ca6
anevaraz <danielgutierrez.info@gmail.com>
2022-09-03 18:17:40 -0300

    set implicity body by dto into swagger

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,8 +1,9 @@
 import { Controller, Post, Req, UseGuards } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
-import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
-import { UnauthorizedUserSwagger } from '../app/users/swagger/unauthorized.user.swagger';
+import { ApiBody, ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { UnauthorizedUserSwagger } from './swagger/unauthorized.auth.swagger';
 import { AuthService } from './auth.service';
+import { LoginUserDto } from '../app/users/dto/login-user.dto';
 
 @Controller('v1/auth')
 @ApiTags('Auth')
@@ -10,6 +11,7 @@ export class AuthController {
   constructor(private readonly authService: AuthService) {}
   @UseGuards(AuthGuard('local'))
   @Post('login')
+  @ApiBody({ type: LoginUserDto })
   @ApiOperation({ summary: 'users login to generate token' })
   @ApiResponse({ status: 201, description: 'token generated' })
   @ApiResponse({ status: 401, type: UnauthorizedUserSwagger })
