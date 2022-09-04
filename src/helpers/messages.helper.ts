b217badcfd65956fc3e8b9eaf8ecc62473a396ea
anevaraz <danielgutierrez.info@gmail.com>
2022-09-04 17:35:19 -0300

    added messages to helper

--- src/helpers/messages.helper.ts
+++ src/helpers/messages.helper.ts
@@ -3,5 +3,7 @@ export const MessagesHelper = {
     'password must contain uppercase and lowercase letters, numbers and special characters',
   PASSWORD_OR_EMAIL_INVALID: 'email and/or password are invalid',
   DATE_OF_BIRTH_VALID: 'date must be standard format DD/MM/YYYY',
-  NOT_FOUND: 'user not found',
+  NOT_FOUND: 'User not found',
+  UNAUTHORIZED: 'Unauthorized apikey',
+  FORBIDDEN: 'Forbidden jwt',
 };
