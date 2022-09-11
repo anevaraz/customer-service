fec085aaf929877fc1b13e9006f09513525a7892
anevaraz <danielgutierrez.info@gmail.com>
2022-09-11 17:46:35 -0300

    added postcode valid message

--- src/helpers/messages.helper.ts
+++ src/helpers/messages.helper.ts
@@ -3,6 +3,7 @@ export const MessagesHelper = {
     'password must contain uppercase and lowercase letters, numbers and special characters',
   PASSWORD_OR_EMAIL_INVALID: 'email and/or password are invalid',
   DATE_OF_BIRTH_VALID: 'date must be standard format DD/MM/YYYY',
+  POSTCODE_VALID: 'postcode must contain only numbers and 8 characters',
   NOT_FOUND: 'Entity not found',
   UNAUTHORIZED: 'Invalid or missing apiKey',
   FORBIDDEN: 'Invalid or missing JWT',
